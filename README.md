23705c279f22005fbc81d21d2d6a5ed857e83d3c
iame <iambolbi0@gmail.com>
2021-06-25 11:06:14 +0000

    Ajoute de liste sur la div des listes 25/06/2021

---
+++ README.md
@@ -0,0 +1,23 @@
+
+Then you can delete these lines !
+<!-- ABOUT THE PROJECT -->
+This projetct is a spot application 
+
+<!-- USAGE EXAMPLES -->
+## Usage
+
+To use my appp, enter in input your task and click validate and this adds to your list
+
+
+<!-- CONTACT -->
+## Contact
+
+Email : iambolbi0@gmail.com
+
+Project Link: https://github.com/iambolbi/todolist.git
+
+
+<!-- INFORMATION -->
+ 
+the application is in progress
+
--- js.js
+++ js.js
@@ -1,5 +1,5 @@
 'use strict'
-
+var tache=[];
 var date = document.getElementById("date");
 var datee= new Date();
 var Datee=datee.getDate()+"/"+datee.getMonth()+"/"+datee.getFullYear();
@@ -9,10 +9,37 @@ function check()
 {
     var task=document.getElementById("task").value;
     if(task.length!=0)
-    {
-        var div = document.getElementById("list");
-        div.insertAdjacentHTML('beforeend','<p>'+task+'</p>');
-        div.insertAdjacentHTML('beforeend','<span class=hours>'+Datee+' '+Hours+'</span>')
+    {   
+        tache.push(task);
+        try {
+            
+            localStorage.setItem("tache", JSON.stringify(tache));
+            
+
+        } catch(e){
+            alert("erreur");
+        }
+        try {
+            
+            let tachee =JSON.parse(localStorage.getItem("tache"));
+            console.log(tachee.length);
+            
+            var div = document.getElementById("list"); 
+            div.insertAdjacentHTML('beforeend','<p>'+tachee[tachee.length-1]+'</p>');
+            div.insertAdjacentHTML('beforeend','<span class=hours>'+Datee+' '+Hours+'</span>')
+
+
+        } catch(e){
+            alert("erreur2");
+        }
+        
+        // let arr = [1,2,3];
+        // localStorage.setItem("key", JSON.stringify(arr));
+
+        // let obj = JSON.parse(localStorage.getItem("key"));
+
+        // console.log(obj[0]);
+        
        
     }
     else
